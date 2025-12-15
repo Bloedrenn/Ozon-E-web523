f1301a07e9518788b3712427f28470e2e79d6d46
Bloedrenn <bloedrenn@gmail.com>
2025-12-15 21:05:32 +0300

    feat(items): добавил иконки (edit и delete) в ItemCard

--- frontend/src/components/Item/ItemCard/ItemCard.jsx
+++ frontend/src/components/Item/ItemCard/ItemCard.jsx
@@ -1,5 +1,10 @@
+import { IoCloseCircleSharp } from 'react-icons/io5';
+import { FaEdit } from "react-icons/fa";
+
 const ItemCard = ({ item }) => (
   <div className="item">
+    <IoCloseCircleSharp className='delete-icon' size={15} />
+    <FaEdit className='edit-icon' size={15} />
     <strong>{item.name}</strong>
     <div>{item.description}</div>
     <div>{item.isAvailable ? 'В наличии :)' : 'Нет в наличии :('}</div>
